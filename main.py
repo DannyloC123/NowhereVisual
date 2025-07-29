e2e2c2ee0df7366be12c271432b4c245f7952c48
Dannylo Correia <DannyloCorreia2006@gmail.com>
2025-07-28 20:48:20 -0400

    Adding schedule library

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import schedule
 import time
+from hashtags import hashtags
 
+
+def post():
+    print('Creating Post')
+
+
+schedule.every(3).seconds.do(post)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
